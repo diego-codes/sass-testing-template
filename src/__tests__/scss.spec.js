d07be31c370f4480b59607e27a0bd186893ce7b2
diego-codes <Diego.Hernandez@ibm.com>
2018-11-20 10:17:16 -0600

    refactor(test): put test code in it's own directory

--- src/__tests__/scss.spec.js
+++ src/__tests__/scss.spec.js
@@ -5,7 +5,7 @@ const glob = require('glob')
 describe('Sass', () => {
   // Find all of the Sass files that end in `*.spec.scss` in any directory in this project.
   // I use path.resolve because True requires absolute paths to compile test files.
-  const sassTestFiles = glob.sync(path.resolve(__dirname, '../**/*.spec.scss'))
+  const sassTestFiles = glob.sync(path.resolve(process.cwd(), 'src/**/*.spec.scss'))
   
   // Run True on every file found with the describe and it methods provided
   sassTestFiles.forEach(file =>
